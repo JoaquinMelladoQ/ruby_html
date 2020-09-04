565e2ecd1d8e70fd18b674caa9f9c6292aad2f82
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-04 18:14:56 -0400

    adding require

---
+++ web.rb
@@ -0,0 +1,11 @@
+require_relative 'components'
+
+text_buttons = ['ver mi web', 'ir a otro lado', 'encargar pizza', 'reclamos', 'jugar']
+
+my_buttons = buttons( text_buttons )
+
+#concatenando todo
+
+index = head() + my_buttons + foot()
+
+File.write('./index.html', index)
