911d3c91bb73b003c45b97fbe59043555dd76036
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-04 18:30:15 -0400

    adding 2 more buttons

--- web.rb
+++ web.rb
@@ -1,6 +1,6 @@
 require_relative 'components'
 
-text_buttons = ['ver mi web', 'ir a otro lado', 'encargar pizza', 'reclamos', 'jugar']
+text_buttons = ['ver mi web', 'ir a otro lado', 'encargar pizza', 'reclamos', 'jugar', 'clases', 'ayudantia']
 
 my_buttons = buttons( text_buttons )
 
